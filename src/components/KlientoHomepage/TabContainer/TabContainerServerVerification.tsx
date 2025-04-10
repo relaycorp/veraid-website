15da69e72172e3be31cc0da7e7e1312e595d7fa9
Maribel Ferreira <maribelferreira@users.noreply.github.com>
2025-04-10 13:57:08 +0100

    Update paragraph styles in TabContainer components for improved readability

--- src/components/KlientoHomepage/TabContainer/TabContainerServerVerification.tsx
+++ src/components/KlientoHomepage/TabContainer/TabContainerServerVerification.tsx
@@ -3,9 +3,8 @@ import TabContainer from "./TabContainer";
 import type { Tab } from "./TabContainer";
 import CodeBlock from "./CodeBlock";
 import JavaScriptIcon from "../../../assets/icons/JavaScript.svg?react";
-import GitHubIcon from "../../../assets/icons/github.svg?react";
 
-const PARAGRAPH = "px-4 text-base leading-normal pb-4";
+const PARAGRAPH = "px-4 text-xs lg:text-base leading-normal pb-4";
 const CONTENT_CONTAINER = "text-left pt-4";
 
 const TabContainerServerVerification: React.FC = () => {
