c0fc61f82b886d02dcadd39e7dca9f910a72735b
Maribel Ferreira <35583455+maribelferreira@users.noreply.github.com>
2025-04-14 16:02:25 +0100

    Tweak kliento homepage (#32)
    VMKTG-32
    
    - [x] Header title: Use a single gradient.
    - [x] Text gradient: Start from lighter colour.
    - [x] Service account input field: Vertically centre text.
    - [x] Remove global CSS for the scrolling bar
    - [x] Add link style for all kliento related markdown pages
    - [x] Add font size for small viewport for the code snippets
    
    ---------
    
    Co-authored-by: Maribel Ferreira <maribelferreira@users.noreply.github.com>

--- src/components/KlientoHomepage/TabContainer/TabContainerClientIntegration.tsx
+++ src/components/KlientoHomepage/TabContainer/TabContainerClientIntegration.tsx
@@ -5,7 +5,7 @@ import CodeBlock from "./CodeBlock";
 import JavaScriptIcon from "../../../assets/icons/JavaScript.svg?react";
 import GitHubIcon from "../../../assets/icons/github.svg?react";
 
-const PARAGRAPH = "px-4 text-xs lg:text-base leading-normal pb-4";
+const PARAGRAPH = "px-4 text-xs sm:text-sm lg:text-base leading-normal pb-4";
 const CONTENT_CONTAINER = "text-left pt-4 bg-black";
 
 const TabContainerClientIntegration: React.FC = () => {
